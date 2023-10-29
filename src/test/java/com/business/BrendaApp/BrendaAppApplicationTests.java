c00b1c96ec436898c88620bc85ebace5d824581a
cedrickkouek <cedrickkouek@gmail.com>
2023-10-29 02:46:12 +0100

    Done with the basic level

--- src/test/java/com/business/BrendaApp/BrendaAppApplicationTests.java
+++ src/test/java/com/business/BrendaApp/BrendaAppApplicationTests.java
@@ -3,7 +3,9 @@ package com.business.BrendaApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.business.brendaapp.BrendaAppApplication;
+
+@SpringBootTest(classes = BrendaAppApplication.class)
 class BrendaAppApplicationTests {
 
 	@Test
